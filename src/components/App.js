054c641edae1ccf0f3e961a6baa0006a8ccba3a7
gramulos <gramulos@gmail.com>
2016-01-12 15:27:52 +0400

    less files added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Footer from './Footer';
+
+import './App.less';
 
 export default class App extends Component {
     render() {
         return (
-            <div className="container">
+            <div>
                 <Header />
                 Body
+                <Footer />
             </div>
         );
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
+import './Header.less';
+
 export default class Header extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return(
-            <div className="row">
-            <div className="col-md-12">Header</div>
+        return (
+            <div className="header">
+                <div className="container">
+                    <div className="col-md-12">Header</div>
+                </div>
             </div>
         );
     }
