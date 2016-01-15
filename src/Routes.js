ef23d8ed4e056f10cefb2122d8441269fbe631e1
Azer Safarov <gramulos@gmail.com>
2016-01-15 21:49:16 +0400

    animation added

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactRouter, Router, Route, Link } from 'react-router';
+
+import cTemplate from './pages/client/Template';
+import cProducts from './pages/client/Products';
+
+var Routes = (
+    <Router>
+        <Route path="/" component={cTemplate}>
+            <Route path="products" component={cProducts} />
+        </Route>
+    </Router>
+);
+
+export default Routes;
