fc2c064ffd1e22dbc20977492135e6ed2505fa7a
Azer Safarov <gramulos@gmail.com>
2016-01-21 22:16:51 +0400

    product details page modified

--- src/pages/client/Home.js
+++ src/pages/client/Home.js
@@ -52,25 +52,25 @@ export default class Body extends Component {
                             <p>Every company is founded according to some vales that define its future. We have chosen 4 main values since the foundation of EPSILON GROUPS and the company has been continuing its existence in accordance with these values. The values of our company are quality, customer satisfaction, innovativeness and reasonable prices.</p>
                             <br />
 
-                            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12 eg-full-width-container-sub">
+                            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 eg-full-width-container-sub">
                                 <img src={img_star}/>
                                 <h4>Quality</h4>
                                 <p>Quality is the value that we have to talk about first. Exactly, this value supports the other values. Without quality the existence of other values just vanishes. Taking into account this, we constantly control the quality of our products and the projects that we realize and try to keep the high level of quality. Therefore, we can always confidently guarantee the high quality of our products.</p>
                             </div>
 
-                            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12 eg-full-width-container-sub">
+                            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 eg-full-width-container-sub">
                                 <img src={img_customer} alt=""/>
                                 <h4>Customer satisfaction </h4>
                                 <p>Customer satisfaction takes the main place among the other values. In order to satisfy our customers some norms are accepted in our company so that all our employees follow these norms. The hotline that services continuously for our customers is active to receive any complains and suggestions. The great number of our satisfies customer is accepted as a sign of success that shows how customers satisfaction is highly valued in our company. </p>
                             </div>
 
-                            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12 eg-full-width-container-sub">
+                            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 eg-full-width-container-sub">
                                 <img src={img_lamp} alt=""/>
                                 <h4>Innovativeness</h4>
                                 <p>Innovativeness is one of prior concepts of our company in the areas of technology and programming. These areas advance continuously and in order to keep pace with this development we try to make innovations. Otherwise, to gain any success is less probable. For this purpose, our employees are frequently sent to various seminars, trainings. Of course, as a result of hard work and professional development of our employees effect the continuous advance of our products.</p>
                             </div>
 
-                            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12 eg-full-width-container-sub">
+                            <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 eg-full-width-container-sub">
                                 <img src={img_price} alt=""/>
                                 <h4>Reasonable price</h4>
                                 <p>Reasonable price is the main value that attracts customers. As soon as the production process finishes, the products are packed and shipped safely.</p>
@@ -79,32 +79,6 @@ export default class Body extends Component {
                     </div>
                 </div>
             </div>
-            //<div className="container">
-            //    <div className="eg-content-container">
-            //        <h4 className="bigger">About us</h4>
-            //        <p>EPSILON GROUPS provides systems and services in the fields of intelligent systems, robotic vehicles (including unmanned ground, air, and sea vehicles), robotics and automation, weapons systems, intelligent control systems, intelligent transportation systems, intelligent manufacturing, and other advanced technology for government, industry, and not-for-profit clients.</p>
-            //        <br />
-            //        <h4 className="bigger">Our mission</h4>
-            //        <p>To be in the list of the worlds 10 advanced companies in the area of Robot Technologies.</p>
-            //        <br />
-            //        <h4 className="bigger">Our values</h4>
-            //        <p>Every company is founded according to some vales that define its future. We have chosen 4 main values since the foundation of EPSILON GROUPS and the company has been continuing its existence in accordance with these values. The values of our company are quality, customer satisfaction, innovativeness and reasonable prices.</p>
-            //        <br />
-            //        <h4>Quality</h4>
-            //        <p>Quality is the value that we have to talk about first. Exactly, this value supports the other values. Without quality the existence of other values just vanishes. Taking into account this, we constantly control the quality of our products and the projects that we realize and try to keep the high level of quality. Therefore, we can always confidently guarantee the high quality of our products.</p>
-            //        <br />
-            //        <h4>Customer satisfaction </h4>
-            //        <p>Customer satisfaction takes the main place among the other values. In order to satisfy our customers some norms are accepted in our company so that all our employees follow these norms. The hotline that services continuously for our customers is active to receive any complains and suggestions. The great number of our satisfies customer is accepted as a sign of success that shows how customers satisfaction is highly valued in our company. </p>
-            //        <br />
-            //        <h4>Innovativeness</h4>
-            //        <p>Innovativeness is one of prior concepts of our company in the areas of technology and programming. These areas advance continuously and in order to keep pace with this development we try to make innovations. Otherwise, to gain any success is less probable. For this purpose, our employees are frequently sent to various seminars, trainings. Of course, as a result of hard work and professional development of our employees effect the continuous advance of our products.</p>
-            //        <br />
-            //        <h4>Reasonable price</h4>
-            //        <p>Reasonable price is the main value that attracts customers. As soon as the production process finishes, the products are packed and shipped safely.</p>
-            //
-            //        <p>As a result of innovative products with high quality that are produced by our professional team and sales for a reasonable price, we gain customer's satisfaction.</p>
-            //    </div>
-            //</div>
         );
     }
 }
--- src/pages/client/ProductDetails.js
+++ src/pages/client/ProductDetails.js
@@ -1,57 +1,102 @@
 import React, { Component } from 'react';
 
 import './ProductsDetails.less';
+import app_image from '../../images/software/vt/vtAppMain.png';
+import app_image_2 from '../../images/software/vt/2.jpg';
+import app_image_train from '../../images/software/vt/train.jpg';
+import app_image_train2 from '../../images/software/vt/train2.jpg';
+import app_image_shooting_results from '../../images/software/vt/shooting_results.jpg';
 
 export default class ProductDetails extends Component {
     render() {
         return (
             <div className="container">
-                <h2>The code name is "VT".</h2>
+                <div className="row no-margin eg-product-details">
+                    <h2>Military shooting simulator</h2>
 
-                <p>Virtual trainer is assigned to train people for the purpose of enhancing their military skills and
-                    musketry.</p>
+                    <div className="row eg-product-container">
+                        <p>Virtual trainer is assigned to train people for the purpose of enhancing their military skills and musketry.</p>
 
-                <p>Virtual trainer allows to:</p>
-                <ul>
-                    <li>Learn the basic rules of shooting</li>
-                    <li>Get introduced with the parts of weapon</li>
-                    <li>Study the security issues</li>
-                    <li>Imitate shooting for the distance of 25m for the pistol Glock</li>
-                    <li>Imitate shooting for the distance of 400m for the gun Kalashnikov</li>
-                    <li>Practice shooting from different positions while moving from one point to the other one</li>
-                </ul>
-                <p>Functional possibilities</p>
-                <ul>
-                    <li>Practice of 3 trainees simultaneously and displaying the personal results of each shooter at the
-                        same time as soon as the training is over
-                    </li>
-                    <li>Fixed, moving, appearing and disappearing objects</li>
-                    <li>Wireless technology enables to shoot from different positions while moving advantages</li>
-                    <li>Entirely secure- the laser gun with visible and non-visible emissions</li>
-                    <li>The laser gun belongs to 1st class of laser products</li>
-                    <li>The use of wireless technologies</li>
-                    <li>Simple, comfortable and secure in use</li>
-                    <li>Special training is not required for teachers.</li>
-                </ul>
-                <p>Work principle</p>
+                        <div className="col-lg-7 col-md-8 col-sm-12 col-xs-12">
 
-                <p>Video projector of a trainer receives the pictures from the computer and displays them on the screen.
-                    The shooter shoots the targets appeared on the screen. On the time of shoot, the camera defines the
-                    coordinates of the laser point and transfers them to the computer, where the calculations are
-                    operated. Acoustic system creates the real sound of a gun. The result of each shoot is shown on the
-                    screen. The final mark for the shoot depends on accuracy and speed. The result of shoots are saved
-                    in protocol and can be printed in the future</p>
+                            <p><b>Virtual trainer allows</b></p>
+                            <ul>
+                                <li>Learn the basic rules of shooting</li>
+                                <li>Get introduced with the parts of weapon</li>
+                                <li>Study the security issues</li>
+                                <li>Imitate shooting for the distance of 25m for the pistol Glock</li>
+                                <li>Imitate shooting for the distance of 400m for the gun Kalashnikov</li>
+                                <li>Practice shooting from different positions while moving from one point to the other one</li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-5 col-md-4 col-sm-12 col-xs-12 eg-product-image">
+                            <img src={app_image}/>
+                        </div>
+                    </div>
 
-                <p>Set</p>
-                <ul>
-                    <li>Model of pistol Glok</li>
-                    <li>Model of gun Kalashnikov</li>
-                    <li>Maser model for Glok</li>
-                    <li>Laser model for Kalashnikov</li>
-                    <li>Training program</li>
-                </ul>
-                <p>Virtual trainer can be placed in a standart rooms. During the lessons, the models of a pistol and a
-                    gun are used. Standart shooting exercises are given in a software. </p>
+                    <div className="row eg-product-container">
+                        <div className="col-lg-7 col-md-8 col-sm-12 col-xs-12">
+                            <p><b>Functional possibilities</b></p>
+                            <ul>
+                                <li>Practice of 3 trainees simultaneously and displaying the personal results of each shooter at the same time as soon as the training is over</li>
+                                <li>Fixed, moving, appearing and disappearing objects</li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-5 col-md-4 col-sm-12 col-xs-12 eg-product-image">
+                            <img src={app_image_2}/>
+                        </div>
+                    </div>
+
+                    <div className="row eg-product-container">
+                        <div className="col-lg-7 col-md-8 col-sm-12 col-xs-12">
+                            <ul>
+                                <li>Wireless technology enables to shoot from different positions while moving advantages</li>
+                                <li>Entirely secure- the laser gun with visible and non-visible emissions</li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-5 col-md-4 col-sm-12 col-xs-12 eg-product-image">
+                            <img src={app_image_train}/>
+                        </div>
+                    </div>
+
+                    <div className="row eg-product-container">
+                        <div className="col-lg-7 col-md-8 col-sm-12 col-xs-12">
+                            <ul>
+                                <li>The laser gun belongs to 1st class of laser products</li>
+                                <li>The use of wireless technologies</li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-5 col-md-4 col-sm-12 col-xs-12 eg-product-image">
+                            <img src={app_image_train2}/>
+                        </div>
+                    </div>
+
+                    <div className="row eg-product-container">
+                        <div className="col-lg-7 col-md-8 col-sm-12 col-xs-12">
+                            <ul>
+                                <li>Simple, comfortable and secure in use</li>
+                                <li>Special training is not required for teachers.</li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-5 col-md-4 col-sm-12 col-xs-12 eg-product-image">
+                            <img src={app_image_shooting_results}/>
+                        </div>
+                    </div>
+
+                    <p>Work principle</p>
+
+                    <p>Video projector of a trainer receives the pictures from the computer and displays them on the screen. The shooter shoots the targets appeared on the screen. On the time of shoot, the camera defines the coordinates of the laser point and transfers them to the computer, where the calculations are operated. Acoustic system creates the real sound of a gun. The result of each shoot is shown on the screen. The final mark for the shoot depends on accuracy and speed. The result of shoots are saved in protocol and can be printed in the future</p>
+
+                    <p>Set</p>
+                    <ul>
+                        <li>Model of pistol Glok</li>
+                        <li>Model of gun Kalashnikov</li>
+                        <li>Laser model for Glok</li>
+                        <li>Laser model for Kalashnikov</li>
+                        <li>Training program</li>
+                    </ul>
+                    <p>Virtual trainer can be placed in a standart rooms. During the lessons, the models of a pistol and agun are used. Standart shooting exercises are given in a software. </p>
+                </div>
             </div>
         );
     }
